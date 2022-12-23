5981c3e8a95cc2a30fd6f9334e1eaf5ac08d59a8
SantaCruz77 <steveyamada66@gmail.com>
2022-12-23 20:47:00 +0900

    Implementation on the admin's side of the product

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
   devise_for :customers, controllers: {
     sessions: 'customer/sessions',
     registrations: 'customer/registrations'
   }
-  devise_for :admins, controllers: {
-    sessions: 'admin/sessions'
-  }
+
   root to: 'pages#home'
+
+  namespace :admin do
+    resources :products, only: %i[index show new create edit update]
+  end
 
   get '/up/', to: 'up#index', as: :up
   get '/up/databases', to: 'up#databases', as: :up_databases
